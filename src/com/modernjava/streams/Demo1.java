a678104470e0ffe729cb71caf67d19db48068b4a
NightOutCoderAtGit <notalkonlycode@gmail.com>
2025-02-13 06:27:51 +0530

    stream creation ways

--- src/com/modernjava/streams/Demo1.java
+++ src/com/modernjava/streams/Demo1.java
@@ -3,6 +3,8 @@ package com.modernjava.streams;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
 
 public class Demo1 {
     public static void main(String[] args) {
@@ -31,5 +33,38 @@ public class Demo1 {
         List<Integer> numsList1 = Arrays.asList(1,2,3,4,5,7,8,9,9,9,9);
         numsList1.stream().distinct().filter(x -> x > 4).peek(System.out::println).map(data -> data * 2).sorted().forEach(System.out::println);
         
+
+        System.out.println("------------Ways of Creating Stream ---------------");
+        /* Other way of creating a stream */
+        Stream<Integer> myStream = Stream.empty();
+         myStream.forEach(System.out::println);
+
+         myStream = Stream.of();
+         myStream.forEach(System.out::println);
+
+         List<Integer> listS1 = Arrays.asList(1,2,3);
+         List<Integer> listS2 = Arrays.asList(101,201,301);
+         Stream<List<Integer>> resultStream = Stream.of(listS1,listS2);
+         resultStream.forEach(System.out::println);
+
+         System.out.println("---------------------------");
+         Stream<Integer> sInteger1 = Stream.of(102,104,106);
+         List<Integer> integer = Arrays.asList(1001,2002);
+         Stream<Integer> sInteger2 = integer.stream();
+         Stream<Integer> intStream = Stream.concat(sInteger1, sInteger2);
+         intStream.forEach(System.out::println);
+
+         System.out.println("---------------------------");
+         Stream<Integer> result = Stream.iterate(13, input -> input + 1).limit(5);
+         result.forEach(System.out::println);
+
+         System.out.println("---------------------------");
+         Stream<Integer> results = Stream.generate(() -> (new Random()).nextInt(10)).limit(5);
+         results.forEach(System.out::println);
+
+         System.out.println("------------reduce terminal example ---------------");
+         List<Integer> dataResult = Arrays.asList(112,113,114);
+         int sumResult = dataResult.stream().reduce(0,(a,b) -> (a+b));
+         System.out.println(sumResult);
     }
 }
